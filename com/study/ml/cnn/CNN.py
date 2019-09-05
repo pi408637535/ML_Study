8e606f17b21e69c349f1f9d3c8ba32118f01860c
galy <piguanghua@wordemotion.com>
2019-09-05 10:48:44 +0800

    第一个CNN 验证码项目总算刷过去了，虽说耗费了20几天，但是结果还是好的。

--- com/study/ml/cnn/CNN.py
+++ com/study/ml/cnn/CNN.py
@@ -16,10 +16,16 @@ import os
 import copy,time
 
 
-file_path = '/Users/piguanghua/Downloads/GenPics'
+file_path = '/root/data/GenPics'
 BATCH_SIZE = 2
 EPOCH = 10
 device = t.device("cuda" if t.cuda.is_available() else "cpu")
+
+'''
+参考博客：https://www.cnblogs.com/king-lps/p/8724361.html
+https://www.jianshu.com/p/08e9d2669b42
+
+'''
 
 
 # Load data
@@ -107,17 +113,25 @@ class nCrossEntropyLoss(nn.Module):
 
     def forward(self, output, label):
 
-        batch_label = label.view(-1, 4)
+        batch_label = label.view(-1,1)
         batch_output = output.view(-1, 4, 10)
+        self.total_loss = 0
 
 
         for i in range(BATCH_SIZE):
             for batch_label_item in batch_label[i]:
                 label_batch_item = []
 
+                str_element = str(batch_label_item.numpy().tolist())
+                for label_item in range(len(str_element)):
+                    label_batch_item.append(str_element[label_item])
+
+                label_batch_item = list(map(int, label_batch_item))
+                self.total_loss += self.loss(batch_output[i], t.LongTensor(label_batch_item))
+        return self.total_loss
 
 
-
+        '''
         output_t = output[:, 0:10]
         label = t.tensor(t.LongTensor(label.data.cpu().numpy())).to(device)
         label_t = label[:, 0]
@@ -128,6 +142,7 @@ class nCrossEntropyLoss(nn.Module):
             self.total_loss = self.loss(output_t, label_t)
 
         return self.total_loss
+        '''
 
 
 def equal(np1, np2):
@@ -138,57 +153,89 @@ def equal(np1, np2):
 
     return n
 
-net = CNN().to(device)
-optimizer = t.optim.Adam(net.parameters(), lr=0.001)
-#loss_func = nn.CrossEntropyLoss()
-loss_func = nCrossEntropyLoss()
 
-best_model_wts = copy.deepcopy(net.state_dict())
-best_acc = 0.0
 
-since = time.time()
-for epoch in range(EPOCH):
+def train():
+    net = CNN().to(device)
 
-    running_loss = 0.0
-    running_corrects = 0
+    optimizer = t.optim.SGD(net.parameters(), lr=0.001)
+    # loss_func = nn.CrossEntropyLoss()
+    loss_func = nCrossEntropyLoss()
+
+    best_model_wts = copy.deepcopy(net.state_dict())
+    best_acc = 0.0
+
+    since = time.time()
+    for epoch in range(EPOCH):
+
+        running_loss = 0.0
+        running_corrects = 0
+
+        for step, (inputs, label) in enumerate(dataloader):
+            pred = t.LongTensor(BATCH_SIZE, 1).zero_()
+            inputs = t.tensor(inputs).to(device)  # (bs, 3, 60, 240)
+            label = t.tensor(label).to(device)  # (bs, 4)
+
+            optimizer.zero_grad()
+
+            output = net(inputs)  # (bs, 40)
+            print(label)
+            loss = loss_func(output, label)
+
+            '''
+            for i in range(4):
+                pre = F.log_softmax(output[:, 10 * i:10 * i + 10], dim=1)  # (bs, 10)
+                pred = t.cat((pred, pre.data.max(1, keepdim=True)[1].cpu()), dim=1)  #
+            '''
+            loss.backward(retain_graph=True)
+            optimizer.step()
+
+            print("loss=", loss.data.numpy().tolist())
+            running_loss += loss.data.numpy().tolist()
+            # running_corrects += equal(pred.numpy()[:, 1:], label.data.cpu().numpy().astype(int))
+
+        epoch_loss = running_loss / dataset_size
+        # epoch_acc = running_corrects / dataset_size
+
+        '''
+        if epoch_acc > best_acc:
+            best_acc = epoch_acc
+            best_model_wts = copy.deepcopy(net.state_dict())
+        '''
+        if epoch == EPOCH - 1:
+            t.save(net.state_dict(), '/root/data/model/cnn.pkl')
+
+        print("end")
+
+        time_elapsed = time.time() - since
+        print('Training complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
+        print('Train Loss:{:.4f} Acc: {:.4f}'.format(epoch_loss, 4.5))
+
+
+def test():
+    net = CNN().to(device)
+    net.load_state_dict(t.load('/root/data/model/cnn.pkl'))
+    num = 0
 
     for step, (inputs, label) in enumerate(dataloader):
+        inputs = t.tensor(inputs).to(device)
+        label = t.tensor(label).to(device)
+        output = net(inputs)
+
+        for i in range(BATCH_SIZE):
+            batch_item = output[i]
+            c0 = np.argmax(batch_item.data.numpy()[0:10])
+            c1 = np.argmax(batch_item.data.numpy()[10:20])
+            c2 = np.argmax(batch_item.data.numpy()[20:30])
+            c3 = np.argmax(batch_item.data.numpy()[30:40])
+            c = '%s%s%s%s' % (c0, c1, c2, c3)
+            print(c,label[i])
+
+        num += 1
+        if num > 5:
+            break
 
 
-
-        pred = t.LongTensor(BATCH_SIZE, 1).zero_()
-        inputs = t.tensor(inputs).to(device)  # (bs, 3, 60, 240)
-        label = t.tensor(label).to(device)  # (bs, 4)
-
-        optimizer.zero_grad()
-
-        output = net(inputs)  # (bs, 40)
-        print(label)
-        loss = loss_func(output, label)
-
-        for i in range(4):
-            pre = F.log_softmax(output[:, 10 * i:10 * i + 10], dim=1)  # (bs, 10)
-            pred = t.cat((pred, pre.data.max(1, keepdim=True)[1].cpu()), dim=1)  #
-
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.data[0] * inputs.size()[0]
-        running_corrects += equal(pred.numpy()[:, 1:], label.data.cpu().numpy().astype(int))
-
-    epoch_loss = running_loss / dataset_size
-    epoch_acc = running_corrects / dataset_size
-
-    if epoch_acc > best_acc:
-        best_acc = epoch_acc
-        best_model_wts = copy.deepcopy(net.state_dict())
-
-    if epoch == EPOCH - 1:
-        t.save(best_model_wts, file_path + '/best_model_wts.pkl')
-
-    print()
-
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Train Loss:{:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
+if __name__ == '__main__':
+    test()
