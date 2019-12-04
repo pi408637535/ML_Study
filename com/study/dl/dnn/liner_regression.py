8b8930faecc53434c02d31a6f14062b9b604cca2
piguanghua <piguanghua@163.com>
2019-12-05 00:36:55 +0800

    今后写模型，模型的参数只能自己算。只有这样才能懂

--- com/study/dl/dnn/liner_regression.py
+++ com/study/dl/dnn/liner_regression.py
@@ -2,17 +2,52 @@ import torch as t
 import numpy as np
 from torch import nn
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+
 
 class LinerRegression(t.nn.Module):
     def __init__(self):
         super(LinerRegression, self).__init__()
-        self.linear = nn.Linear(1, 1)
+        self.linear = nn.Linear(15, 1)
 
     def forward(self, x):
         out = self.linear(x)
         return out
 
 if __name__ == '__main__':
+
+    path_train = "/Users/piguanghua/Downloads/house-prices-advanced-regression-techniques/train.csv"
+    columns = ['MSSubClass', 'LotFrontage', 'LotArea', "OverallQual", "OverallCond", "YearBuilt",
+               "YearRemodAdd", "MasVnrArea", "BsmtFinSF1", "TotalBsmtSF", "GrLivArea", "TotRmsAbvGrd",
+               "GarageYrBlt", "GarageArea", "YrSold", "SalePrice"]
+
+    data = pd.read_csv(path_train, usecols=columns)
+    data["LotFrontage"] = data["LotFrontage"].fillna(np.mean(data["LotFrontage"]))
+    data["GarageYrBlt"] = data["GarageYrBlt"].fillna(np.mean(data["GarageYrBlt"]))
+    data = data.dropna()
+
+    X = data.iloc[:, :-1].values
+    y = data['SalePrice'].values  # 房间价格
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    bins = int(1 + 4 * math.log(len(y)))
+    plt.hist(y, bins=bins, density=0, facecolor="blue", edgecolor="black", alpha=0.7)
+    # 显示横轴标签
+    plt.xlabel("interval")
+    # 显示纵轴标签
+    plt.ylabel("SalePrice")
+    # 显示图标题
+    # plt.title("")
+    plt.show()
+
+    '''
     x_train = np.array([[3.3], [4.4], [5.5], [6.71], [6.93], [4.168],
                         [9.779], [6.182], [7.59], [2.167], [7.042],
                         [10.791], [5.313], [7.997], [3.1]], dtype=np.float32)
@@ -20,13 +55,23 @@ if __name__ == '__main__':
     y_train = np.array([[1.7], [2.76], [2.09], [3.19], [1.694], [1.573],
                         [3.366], [2.596], [2.53], [1.221], [2.827],
                         [3.465], [1.65], [2.904], [1.3]], dtype=np.float32)
+    '''
+    scaler_x = StandardScaler()
+    scaler_y = StandardScaler()
+
+    X = scaler_x.fit_transform(X)
+    y = scaler_y.fit_transform(y.reshape((-1, 1)))
+
+    x_train = X
+    y_train = y
+
 
     model = LinerRegression()
     criterion = nn.MSELoss()
     optimizer = t.optim.SGD(model.parameters(), lr = 1e-4)
 
-    x_train = t.from_numpy(x_train)
-    y_train = t.from_numpy(y_train)
+    x_train = t.FloatTensor(x_train)
+    y_train = t.FloatTensor(y_train)
 
     num_epochs = 1000
     for epoch in range(num_epochs):
@@ -39,11 +84,7 @@ if __name__ == '__main__':
         if (epoch + 1) % 20 == 0:
             print(f'Epoch[{epoch + 1}/{num_epochs}], loss: {loss.item():.6f}')
 
-    predict = model(x_train)
-    plt.plot(x_train.numpy(), y_train.numpy(), 'ro', label='Original data')
-    plt.plot(x_train.numpy(), predict.data.numpy(), label='Fitting Line')
 
 
-    #plt.scatter(x_train, y_train)
-    plt.show()
+
 
---
+++ com/study/dl/rnn/rnn_sin_cos.py
@@ -0,0 +1,101 @@
+import torch as t
+import torchvision as tv
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+import argparse
+import math
+import csv
+import cv2
+import numpy as np
+import pandas as pd
+from PIL import Image
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+'''
+1.通过输入sin值去预测cos
+'''
+
+# Hyper Parameters
+TIME_STEP = 10  # rnn time step
+INPUT_SIZE = 1  # rnn input size
+LR = 0.02  # learning rate
+
+
+
+class MyRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(MyRNN, self).__init__()
+        self.rnn = nn.RNN(
+            input_size = input_size,
+            hidden_size = hidden_size,
+            batch_first = True
+        )
+        self.out = nn.Linear(hidden_size, output_size)
+
+    '''
+    :param x:  batch,seq_len,input_size: 1,10,1
+    :param h_state: batch, h_size
+    :return:
+        因为本函数做作的是的，输入con_y来模拟sin_y，所以每个input的prediction都要
+    作为损失的依据。
+    '''
+    def forward(self, x, h_state):
+        #output: batch, seq_len,direction * h_size: 1,10,32
+        #h_state:batch,layer*direction, h_size : 1,1,32
+
+        output,h_n = self.rnn(x, h_state)
+
+        out = self.out(t.squeeze(output))
+        return out,h_state
+
+
+def repackage_hidden(h):
+    if isinstance(h, t.Tensor):
+        return h.detach()
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+
+if __name__ == '__main__':
+    input_size = 1
+    hidden_size = 32
+    output_size = 1
+
+    rnn = MyRNN(input_size, hidden_size, output_size)
+    optimizer = optim.Adam(rnn.parameters(), lr=LR)
+    loss_func = nn.MSELoss()
+
+    h_state = t.zeros(1,1,32)
+
+    predict_list = []
+    y_list = []
+    steps_list = []
+
+    for step in range(60):
+        start, end = step * np.pi, (step + 1) * np.pi
+        steps = np.linspace(start, end, TIME_STEP, dtype=np.float32)
+        x_np = np.sin(steps)
+        y_np = np.cos(steps)
+
+        x = t.Tensor(t.from_numpy(x_np[np.newaxis, :, np.newaxis]))
+        y = t.Tensor(t.from_numpy(y_np[np.newaxis, :, np.newaxis]))
+
+        #x shape batch,seq_len,dim : 1,10,1
+        h_state = repackage_hidden(h_state)
+        predict, h_n = rnn(x, h_state)
+        #h_state = repackage_hidden(h_state)
+        loss = loss_func(predict, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        steps_list.extend(steps.tolist())
+        predict_list.extend(predict.data.numpy().flatten())
+        y_list.extend(y_np.flatten())
+
+    plt.plot(steps_list, y_list, 'r-')
+    plt.plot(steps_list, predict_list, 'b-')
+    plt.draw()
+    plt.show()
