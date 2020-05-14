dd3c1610998be98449ff1af980ecf563ec613dee
piguanghua <piguanghua@163.com>
2020-05-14 17:00:52 +0800

    seq2seq数据和模型数据调试通

--- com/study/dl/seq2seq/transformer_four.py
+++ com/study/dl/seq2seq/transformer_four.py
@@ -31,6 +31,116 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 
+#hypotrical parameter
+USE_CUDA = t.cuda.is_available()
+
+# 为了保证实验结果可以复现，我们经常会把各种random seed固定在某一个值
+random.seed(53113)
+np.random.seed(53113)
+t.manual_seed(53113)
+if USE_CUDA:
+    t.cuda.manual_seed(53113)
+UNK_IDX = 0
+PAD_IDX = 1
+NUM_EPOCHS = 1
+BATCH_SIZE = 32  # the batch size
+LEARNING_RATE = 1e-3  # the initial learning rate
+EPOCHS = 10
+
+EN_WORD2ID = {}
+EN_ID2WORD = {}
+CH_WORD2ID = {}
+CH_ID2WORD = {}
+EN_VOCAB = None
+CH_VOCAB = None
+MAX_LEN = 25
+
+def pre_train():
+    global EN_WORD2ID, EN_ID2WORD, CH_WORD2ID, CH_ID2WORD, EN_VOCAB, CH_VOCAB, MAX_LEN
+
+    def build_dict(sentences, max_words=21116):
+        word_count = Counter()
+        for sentence in sentences:
+            for s in sentence:
+                word_count[s] += 1
+        ls = word_count.most_common(max_words)
+        total_words = len(ls) + 2
+        word_dict = {w[0]: index + 2 for index, w in enumerate(ls)}
+        word_dict["UNK"] = UNK_IDX
+        word_dict["PAD"] = PAD_IDX
+        return word_dict, total_words
+
+    train_en = ["Anthony Fauci says the best evidence shows the virus behind the pandemic was not made in a lab in China.".split(" ")]
+    train_cn = ["安东尼·福奇 说，最 有力的 证据 表明，造成 疫情 的 新冠病毒 并 不是 中国的 实验室 人为 制造 的。".split(" ")]
+    en_dict, en_total_words = build_dict(train_en)
+    cn_dict, cn_total_words = build_dict(train_cn)
+    EN_VOCAB = en_total_words
+    CH_VOCAB = cn_total_words
+
+    en_word2id = {k: v for k, v in en_dict.items()}
+    en_id2word = {v: k for k, v in en_dict.items()}
+
+    cn_word2id = {k: v for k, v in cn_dict.items()}
+    cn_id2word = {v: k for k, v in cn_dict.items()}
+
+    EN_WORD2ID, EN_ID2WORD, CH_WORD2ID, CH_ID2WORD = en_word2id, en_id2word, cn_word2id, cn_id2word
+
+    # 把单词全部转变成数字
+    def encode(en_sentences, cn_sentences, en_dict, cn_dict, sort_by_len=False, maxlen=20):
+        '''
+            Encode the sequences.
+        '''
+
+        length = len(en_sentences)
+        out_en_sentences = [[en_dict.get(w, 0) for w in sent] for sent in en_sentences]
+        out_cn_sentences = [[cn_dict.get(w, 0) for w in sent] for sent in cn_sentences]
+
+        # sort sentences by english lengths
+
+        # out_en_sentences = [out_en_sentences[i] for i in out_en_sentences]
+        # out_cn_sentences = [out_cn_sentences[i] for i in out_cn_sentences]
+
+        return out_en_sentences, out_cn_sentences
+
+    train_en, train_cn = encode(train_en, train_cn, en_dict, cn_dict)
+    #dev_en, dev_cn = encode(dev_en, dev_cn, en_dict, cn_dict)
+
+    class CustomDataSet(Dataset):
+
+        def __init__(self, source_sen, target_sen, transform=None):
+            self.data = self._load_dataset(source_sen, target_sen)
+            self._transform = transform
+
+        def _load_dataset(self, source_sen, target_sen):
+
+            def pad_input(sentence, seq_len):
+                length = len(sentence)
+                feature = np.zeros(seq_len, dtype=int)
+                if len(sentence) != 0:
+                    feature[:length] = sentence[:seq_len]
+                return feature
+
+            all_data = []
+            for index, item in enumerate(source_sen):
+                data = {
+                    'source': pad_input(source_sen[index], MAX_LEN),
+                    'source_len': len(source_sen[index]),
+                    'target': pad_input(target_sen[index], MAX_LEN),
+                    'target_len': len(target_sen[index]),
+                }
+                all_data.append(data)
+            return all_data
+
+        def __len__(self):
+            return len(self.data)
+
+        def __getitem__(self, index):
+            return self.data[index]
+
+    train_data_set = CustomDataSet(train_en, train_cn)
+    #dev_data_set = CustomDataSet(dev_en, dev_cn)
+    return train_data_set
+
 
 dim=512
 dk_dim = 64
@@ -60,7 +170,7 @@ def get_attn_subsequent_mask(seq):
     subsequent_mask = torch.from_numpy(subsequent_mask).byte()
     return subsequent_mask
 
-class  ScaledDotProductAttention(nn.Module):
+class ScaledDotProductAttention(nn.Module):
     def __init__(self, d_k):
         super(ScaledDotProductAttention, self).__init__()
         self.d_k = d_k
@@ -85,6 +195,7 @@ class MultiHeadAttention(nn.Module):
         self.head = head
         self.d_k = d_k
         self.atten_model = atten_model
+        self.norm = nn.LayerNorm(512)
 
     #设计失误啊,对于Encoder来说:Q,K,V都来自Encoder但对于Decoder来说Q来自Decoder的input，K,V来自Encoder的output
     def forward(self, q, k, v, atten_mask):
@@ -109,7 +220,7 @@ class MultiHeadAttention(nn.Module):
             view(batch, -1, self.head * self.d_k)
         Z =  self.Wz(context)
 
-        return nn.LayerNorm(512)(Z + q),atten
+        return self.norm(Z + q),atten
 
 class PoswiseFeedForwardNet(nn.Module):
     def __init__(self, dim):
@@ -144,12 +255,13 @@ class EncoderLayer(nn.Module):
 
 
 class Encoder(nn.Module):
-    def __init__(self, vocab, dim, d_k, head):
+    def __init__(self, vocab, dim, d_k, head, device):
         super(Encoder, self).__init__()
         self.embed = nn.Embedding(vocab, dim)
         #self.pos_emb = nn.Embedding.from_pretrained(get_sinusoid_encoding_table(src_len + 1, dim), freeze=True)
         self.layers = nn.ModuleList([EncoderLayer(MultiHeadAttention(dim, d_k, head, ScaledDotProductAttention(d_k)),
                                                   PoswiseFeedForwardNet(dim) ) for _ in range(head)])
+        self.device = device
 
     def forward(self, enc_inputs):  # enc_inputs : [batch_size x source_len]
         #enc_outputs = self.embed(enc_inputs) +
@@ -157,6 +269,7 @@ class Encoder(nn.Module):
 
         #?enc_self_attn_mask:batch,seq,seq
         enc_self_attn_mask = get_attn_pad_mask(enc_inputs, enc_inputs)
+        enc_self_attn_mask = enc_self_attn_mask.to(device)
         enc_self_attens = []
         for layer in self.layers:
             enc_outputs, enc_self_attn = layer(enc_outputs, enc_self_attn_mask)
@@ -183,13 +296,14 @@ class DecoderLayer(nn.Module):
         return enc_outputs, deco_attn, dec_enc_attn
 
 class Decoder(nn.Module):
-    def __init__(self, vocab, dim, d_k, head):
+    def __init__(self, vocab, dim, d_k, head, device):
         super(Decoder, self).__init__()
         self.embed = nn.Embedding(vocab, dim)
         #self.pos_emb = nn.Embedding.from_pretrained(get_sinusoid_encoding_table(src_len + 1, dim), freeze=True)
         self.layers = nn.ModuleList([DecoderLayer(MultiHeadAttention(dim, d_k, head, ScaledDotProductAttention(d_k)),
                                                   MultiHeadAttention(dim, d_k, head, ScaledDotProductAttention(d_k)),
                                                   PoswiseFeedForwardNet(dim) ) for _ in range(head)])
+        self.device = device
 
     def forward(self, deco_inputs, enc_outputs):  # enc_inputs : [batch_size x source_len]
         #enc_outputs = self.embed(enc_inputs) +
@@ -197,6 +311,8 @@ class Decoder(nn.Module):
 
         #?enc_self_attn_mask:batch,seq,seq
         deco_self_attn_mask = get_attn_subsequent_mask(deco_inputs)
+        deco_self_attn_mask = deco_self_attn_mask.to(self.device)
+
         deco_self_attens = []
         deco_enc_attns = []
         for layer in self.layers:
@@ -214,23 +330,104 @@ class Transformer(nn.Module):
         self.decoder = decoder
         self.projection = nn.Linear(dim, vocab_size, bias=False)
 
-    def forward(self, text):
-        enc_outputs, enc_self_atten = encoder(data)
-        deco_outputs, deco_self_attens, deco_enc_attns = decoder(data, enc_outputs)
+
+    def forward(self, source, target):
+        enc_outputs, enc_self_atten = encoder(source)
+        deco_outputs, deco_self_attens, deco_enc_attns = decoder(target, enc_outputs)
         dec_logits = self.projection(deco_outputs) #dec_logits:batch,seq,vocab
         return dec_logits, enc_self_atten, deco_self_attens, deco_enc_attns
+
+class LanguageCriterion(nn.Module):
+    def __init__(self):
+        super(LanguageCriterion, self).__init__()
+
+    '''
+    :arg
+        model_target:batch,seq,vocab
+        text_target: batch,seq
+        mask: batch,seq
+    '''
+    def forward(self, model_target, text_target, mask):
+        batch_max_length = mask.shape[1]
+        text_target = text_target[:, :batch_max_length]
+        model_target = model_target[:, :batch_max_length, :]
+        batch,seq = model_target.shape[0],model_target.shape[1]
+        model_target = model_target.contiguous().view(batch * seq, -1)
+        model_target = F.log_softmax(model_target, dim= 1)
+
+        text_target = text_target.contiguous().view(-1,1)
+
+        loss = -t.gather(model_target, dim=1, index = text_target).view(batch,-1) * mask
+        loss = loss.sum() / mask.sum()
+        return loss
+
+def train(model, train_dataloader, optimizer, criterion, epochs, device):
+    model.train()
+    for epoch in range(epochs):
+
+        for batch_id, batch_data in enumerate(train_dataloader):
+            optimizer.zero_grad()
+
+            source = batch_data['source'].to(device)
+            source_len = batch_data['source_len'].to(device)
+            target = batch_data['target'].to(device)
+            target_len = batch_data['target_len'].to(device)
+
+
+
+            dec_logits, enc_self_atten, deco_self_attens, deco_enc_attns = model(source, target)
+
+            batch = target_len.shape[0]
+
+            target_max_len = t.max(target_len).item()
+            mask = t.arange(target_max_len).repeat(batch, 1).to(device) < target_len.view(-1, 1).expand(-1,target_max_len)
+            mask = mask.float()
+            loss = criterion(dec_logits, target, mask)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 5.)
+            optimizer.step()
+
+            if batch_id % 1e3 == 0:
+                print(loss.item())
+
+    '''
+    save_dir = "./model"
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    best_mode_path = '{}/{}.pkl'.format(save_dir,"seq2seq")
+    '''
+
+
+
+    '''
+    torch.save({
+        'model': model.state_dict(),
+        'epoch': epoch
+    }, best_mode_path)
+    '''
 
 
 if __name__ == '__main__':
     batch, seq = 2, 5
     data = t.randint(10, size=(batch , seq))
     vocab, dim, d_k, head = 20, 512, 64, 8
-    encoder = Encoder(vocab, dim, d_k, head)
-    decoder = Decoder(vocab, dim, d_k, head)
+
+
+
+    #enc_outputs, enc_self_atten = encoder(data)
+    #deco_outputs, deco_self_attens, deco_enc_attns = decoder(data, enc_outputs)
+    #dec_logits, enc_self_atten, deco_self_attens, deco_enc_attns = model(data)
+
+    train_dataloader = pre_train()
+    train_dataloader = DataLoader(dataset=train_dataloader, batch_size=BATCH_SIZE)
+
+    device = torch.device('cuda' if USE_CUDA else 'cpu')
+    criterion = LanguageCriterion()
+    encoder = Encoder(vocab, dim, d_k, head, device)
+    decoder = Decoder(vocab, dim, d_k, head, device)
     model = Transformer(encoder, decoder, dim, vocab)
+    optimizer = t.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-    enc_outputs, enc_self_atten = encoder(data)
-    deco_outputs, deco_self_attens, deco_enc_attns = decoder(data, enc_outputs)
-    dec_logits, enc_self_atten, deco_self_attens, deco_enc_attns = model(data)
-    print(dec_logits.shape)
+    model.to(device)
 
+    train(model, train_dataloader, optimizer, criterion, EPOCHS, device)
